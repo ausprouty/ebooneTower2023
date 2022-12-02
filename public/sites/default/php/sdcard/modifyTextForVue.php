7c1b4e38f1fc35169414e02a62b635970f3130e7
ausprouty <bob@hereslife.com>
2022-12-02 16:07:32 +1100

    modify popups

---
+++ public/sites/default/php/sdcard/modifyTextForVue.php
@@ -0,0 +1,53 @@
+<?php
+
+
+
+
+
+function modifyTextForVue($text){
+    $text = str_ireplace('sites/mc2/images/standard/', '@assets/images/', $text);
+    $text = modifyTextForVuePopUp($text)
+    return $text;
+}
+
+/*need to change 
+
+<a href="javascript:popUp('pop1')">1 Timothy 2:4</a>
+	
+to 
+
+<span class="popup" @click = "popUp('pop1')"> 1 Timothy 2:4 </span>
+*/
+
+function modifyTextForVuePopUp($text){
+    $template = <span class="popup" @click = "popUp('[id]')"> [reference]</span>
+    $count = substr_count($text, '<a href="javascript:popUp');
+    $pos_start = 0;
+    for ($i = 1; $i <= $count; $i++){
+        $pos_href_start = strpos($text, '<a href="javascript:popUp');
+        // get id
+        $pos_id_begin = strpos ($text, "'",  $pos_href_start) +1;
+        $pos_id_end = strpos ($text,  "'", $pos_id_begin) -1;
+        $length = $pos_id_end - $pos_id_begin;
+        $popup_id = substr($text,  $pos_id_begin, $length);
+        // get reference
+        $pos_label_begin = strpos($text, '>', $pos_href_start) +1;
+        $pos_label_end = strpos($text, '<', $pos_href_end) -1;
+        $length = $pos_label_begin - $pos_label_end;
+        $reference = substr($text,  $pos_label_begin, $length);
+        $old = array(
+            '[id]',
+            '[reference]'
+        );
+        $change = array(
+            $id,
+            $reference
+        );
+        $new = str_replace($old, $change, $template);
+        $length =  $pos_label_end +4 -  $pos_href_start;
+        $text = substr_replace($text,$new, $pos_href_start,$length);
+    }
+    return $largest;
+
+
+}
