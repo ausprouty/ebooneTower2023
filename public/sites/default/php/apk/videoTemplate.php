0077992fb78d03a23ed6530f4b053902e55b3290
ausprouty <bob@hereslife.com>
2022-12-19 09:52:31 +1100

    video template

--- public/sites/default/php/apk/videoTemplate.php
+++ public/sites/default/php/apk/videoTemplate.php
@@ -1,16 +1,17 @@
 <?php
 
-myRequireOnce ('videoFindForSDCardNewName.php', 'sdcard');
+myRequireOnce('videoFindForSDCardNewName.php', 'sdcard');
 
-function videoTemplateOffline($title_phrase, $p, $offline_video_count, $bookmark){
-    $new = videoTemplateLink();
+function videoTemplateOffline($title_phrase, $p, $offline_video_count, $bookmark)
+{
+    $template = videoTemplateLink();
     $filename = $bookmark['page']->filename;
-    $video = '/media/'. $p['country_code'] . '/'. $p['language_iso'] .'/video/'.  $p['folder_name'] .'/';
-    $video .=  videoFindForSDCardNewName($filename) ;
-    if ($offline_video_count > 0){
-        $video .= '-'. $offline_video_count;
+    $video = '/media/' . $p['country_code'] . '/' . $p['language_iso'] . '/video/' .  $p['folder_name'] . '/';
+    $video .=  videoFindForSDCardNewName($filename);
+    if ($offline_video_count > 0) {
+        $video .= '-' . $offline_video_count;
     }
-    $video .='.mp4';
+    $video .= '.mp4';
     $old = array(
         '[video]',
         '[title_phrase]'
@@ -20,22 +21,23 @@ function videoTemplateOffline($title_phrase, $p, $offline_video_count, $bookmark
         $title_phrase
     );
     $output = str_replace($old, $new, $template);
-    return $output; 
+    return $output;
 }
 
 
-function videoTemplateLink($bookmark){
-    $template_link ='
+function videoTemplateLink($bookmark)
+{
+    $template_link = '
         <button id="VimeoButton0" type="button" class="external-movie ">[title_phrase]</button>
         <div class="collapsed">
         <video id="video"  width = "100%" controls>
         <source src="[video]" type="video/mp4">
         </div>';
     return  $template_link;
-
 }
 
-function videoTemplateWatchPhrase($bookmark){
+function videoTemplateWatchPhrase($bookmark)
+{
     $watch_phrase = $bookmark['language']->watch_offline;
     return $watch_phrase;
 }
