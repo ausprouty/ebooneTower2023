75c4f6c40f927110a8e71e199cf5f199064fd208
ausprouty <bob.prouty@powertochange.org.au>
2023-07-03 14:50:52 +1000

    modify NOte Area changed for MyFriends

--- public/sites/myfriends/php/modifyNoteArea.php
+++ public/sites/myfriends/php/modifyNoteArea.php
@@ -1,46 +1,35 @@
 <?php
 myRequireOnce('writeLog.php');
+myRequireOnce('myGetPrototypeFile.php');
+
 /*
 
-This site always adds the standard instruction
+Input is:
+
+<div class="note-area" id="note1">
+<form id="note1">Notes: (click outside box to save)<br />
+<textarea rows="5"></textarea></form>
+</div>
+
+Output depends on destination
 
 This is an attempt to have a growing note area on the iphone
 SEE https://css-tricks.com/auto-growing-inputs-textareas/
 
-alter notes area from input:
-
-<div class="note-area" id="note#">
-........
-</div>
-
-to:
-
-<div class="note-div" >
-    <form class = "auto_submit_item">
-        Notes: (click outside box to save)<br>
-         <textarea class="textarea resize-ta" onchange= "addNote('[id]')"  id ="[id]" ></textarea>';
-    </form>
-</div>
-
-remove for nojs  (if $p['destination] == 'nojs')
-
   */
 function modifyNoteArea($text,  $bookmark, $p)
 {
-    $standard_instruction = $bookmark['language']->notes;
-    $template = `
-    <div class="note-div">
-        <form class = "auto_submit_item">
-        <p class="note-instruction">[user_instruction]</p>
-            <textarea class="textarea resize-ta" onkeyup= "addNote('[id]')"  id ="[id]" ></textarea>`;
-    if ($p['destination'] == 'nojs' || $p['destination'] == 'pdf') {
+    $template = myGetPrototypeFile('note.html');
+    writeLogDebug('modifyNoteArea-23', $p['destination']);
+    writeLogDebug('modifyNoteArea-24', $template);
+    if ($p['destination'] == 'nojs') {
         $template = '<div class="note-removed">';
     }
     $count = substr_count($text, '<div class="note-area"');
     for ($i = 1; $i <= $count; $i++) {
         $pos_start = strpos($text, '<div class="note-area"');
         $pos_end = strpos($text, '</div>', $pos_start);
-        $length = $pos_end - $pos_start;
+        $length = $pos_end - $pos_start + 6;
         $block = substr($text, $pos_start, $length);
         $row_start = strpos($block, 'rows="') + 6;
         $row_end = strpos($block, '">', $row_start);
@@ -51,20 +40,22 @@ function modifyNoteArea($text,  $bookmark, $p)
             $label_end = strpos($block, '<br />', $label_start);
             $label_length = $label_end - $label_start;
         }
+        $standard_instruction = $bookmark['language']->notes;
         $bad = array(
             '[id]',
             '[rows]',
             '[user_instruction]'
         );
         $good = array(
+
             'note' . $i . 'Text',
             $rows,
             $standard_instruction
         );
         $new_template = str_replace($bad, $good, $template);
-
         // from https://stackoverflow.com/questions/1252693/using-str-replace-so-that-it-only-acts-on-the-first-match
         $text = substr_replace($text, $new_template, $pos_start, $length);
     }
     return $text;
 }
+
