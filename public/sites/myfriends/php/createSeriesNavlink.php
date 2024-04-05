4640bd88d500108970ce8783c55af66bb69d774e
ausprouty <bob.prouty@powertochange.org.au>
2024-04-05 18:58:49 -0500

    navlink for Austria in MyFriends

--- public/sites/myfriends/php/createSeriesNavlink.php
+++ public/sites/myfriends/php/createSeriesNavlink.php
@@ -7,8 +7,10 @@ function createSeriesNavlink($p){
     if ($p['language_iso'] == 'eng' && $p['country_code']== 'AU'){
         $navlink = '../'. $p['library_code'];
     }
-    if ($p['language_iso'] == 'deu' && $p['country_code']== 'AT'){
-        $navlink = '../'. $p['library_code'];
+    if ($p['language_iso'] == 'deu' 
+        && $p['country_code']== 'AT'
+        && $p['library_code'] != 'library'){
+            $navlink = '../'. $p['library_code'] . '.html';
     }
     return $navlink;
 }
