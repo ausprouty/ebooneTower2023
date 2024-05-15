d47d083416b27e358105abdbb32481f4fe96af59
ausprouty <bob.prouty@powertochange.org.au>
2024-05-16 08:59:12 +1000

    LIbrary Title works

--- src/components/library/LibraryBookTitle.vue
+++ src/components/library/LibraryBookTitle.vue
@@ -3,39 +3,32 @@
     <h2>Name and Code</h2>
     <p>#: {{ book.id }}</p>
     <BaseInput
-      v-model="libraryBookTitle"
+      :value="libraryBookTitle"
       label="Title:"
       type="text"
       placeholder="Title"
       class="field"
-      
+      @change="setBookTitle(index, $event.target.value)"
     />
   </div>
 </template>
 <script>
 import Vue from 'vue'
+import BaseInput from '../BaseInput.vue';
 export default {
   props: {
     book: Object,
     index: Number,
   },
+  methods: {
+    setBookTitle(index, value) {
+      //this.$store.state.bookmark.library.books[index].title = value
+      this.$store.commit('setBookTitle', { index, value })
+    },
+  },
   computed: {
-    libraryBookTitle: {
-      get() {
-        var title = this.$store.state.bookmark.library.books[this.index].title
-        return title
-      },
-      set(value) {
-        console.log(this.index)
-        console.log(value)
-        if (!this.$store.state.bookmark.library.books[this.index]) {
-          Vue.set(this.$store.state.bookmark.library.books, this.index, {})
-        }
-        //this.$store.state.bookmark.library.books[this.index].title = value
-        //this.setBookTitle(this.index, value)
-        var index = this.index
-        this.$store.commit('setBookTitle', { index, value })
-      },
+    libraryBookTitle() {
+      return this.$store.state.bookmark.library.books[this.index].title
     },
   },
 }
